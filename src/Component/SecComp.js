510ef349ea34e6bd4c72994b3e8c745e63479147
Prasad Khanapure <pnkhanapure@gmail.com>
2022-08-09 01:50:31 +0530

    Axios Added

--- src/Component/SecComp.js
+++ src/Component/SecComp.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 import ThirdComp from "./ThirdComp";
 
 const SecComp = () => {
@@ -12,15 +13,22 @@ const SecComp = () => {
   };
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos/1")
-      .then((res) => res.json())
-      .then((response) => {
-        console.log(response);
-        setRes(response.title);
-      });
+    // axios.request("https://jsonplaceholder.typicode.com/todos")
+    //   .then((res) => {
+    //   console.log(res);
+    //   });
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      console.log(res.data);
+    });
+    // fetch("https://jsonplaceholder.typicode.com/todos/1")
+    //   .then((res) => res.json())
+    //   .then((response) => {
+    //     console.log(response);
+    //     setRes(response.title);
+    //   });
 
-    console.log(name);
-    console.log();
+    // console.log(name);
+    // console.log();
   }, [flag]); //hook
   // Mount => first load / on load => []
   // Update => first load / on load => [drink]
